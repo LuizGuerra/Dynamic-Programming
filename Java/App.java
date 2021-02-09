4dd68c314ccf6da9b7feaa9f9f147e706abde49d
LuizGuerra <luizpedrofg@gmail.com>
2021-02-08 22:43:38 -0300

    reestructured files

---
+++ Java/App.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main (String args[]) {
+        // fibonacci();
+        // gridTraveler();
+        // canSum();
+        // howSum();
+        bestSum();
+    }
+
+    private static void fibonacci() {
+        Fibonacci f = new Fibonacci();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "th fibonacci number is " +f.fib(i) );
+        }
+        System.out.println("50th fibonacci number is " +f.fib(50) );
+        System.out.println("100th th fibonacci number is " +f.fib(100) );
+    }
+
+    private static void gridTraveler() {
+        GridTraveler gt = new GridTraveler();
+
+        System.out.println("Naive traveling solution:");
+        System.out.println(gt.naiveTravel(2, 2));
+        System.out.println(gt.naiveTravel(2, 3));
+        System.out.println(gt.naiveTravel(3, 3));
+        // System.out.println(gt.naiveTravel(16, 16));
+        
+        System.out.println("\nDynamic traveling solution:");
+        System.out.println(gt.dinamicTravel(2, 2));
+        System.out.println(gt.dinamicTravel(2, 3));
+        System.out.println(gt.dinamicTravel(3, 3));
+        System.out.println(gt.dinamicTravel(18, 18));
+    }
+
+    private static void canSum() {
+        Sum s = new Sum();
+        System.out.println("Naive can sum:");
+        System.out.println(s.naiveCanSum(7, Arrays.asList(2, 3)));
+        System.out.println(s.naiveCanSum(7, Arrays.asList(5,3,4,7)));
+        System.out.println(s.naiveCanSum(7, Arrays.asList(2,4)));
+        System.out.println(s.naiveCanSum(8, Arrays.asList(2,3,5)));
+        // System.out.println(s.naiveCanSum(300, Arrays.asList(7,14)));
+        
+        System.out.println("\nNaive can sum:");
+        System.out.println(s.dynamicCanSum(7, Arrays.asList(2, 3)));
+        System.out.println(s.dynamicCanSum(7, Arrays.asList(5,3,4,7)));
+        System.out.println(s.dynamicCanSum(7, Arrays.asList(2,4)));
+        System.out.println(s.dynamicCanSum(8, Arrays.asList(2,3,5)));
+        System.out.println(s.dynamicCanSum(300, Arrays.asList(7,14)));
+    }
+
+    private static void howSum() {
+        Sum s = new Sum();
+        System.out.println("Naive how sum:");
+        System.out.println(s.naiveHowSum(7, Arrays.asList(2, 3)));
+        System.out.println(s.naiveHowSum(7, Arrays.asList(5,3,4,7)));
+        System.out.println(s.naiveHowSum(7, Arrays.asList(2, 4)));
+        System.out.println(s.naiveHowSum(8, Arrays.asList(2,3,5)));
+        // System.out.println(s.naiveHowSum(300, Arrays.asList(7,14)));
+        
+        System.out.println("\nDynamic how sum:");
+        System.out.println(s.dynamicHowSum(7, Arrays.asList(2, 3)));
+        System.out.println(s.dynamicHowSum(7, Arrays.asList(5,3,4,7)));
+        System.out.println(s.dynamicHowSum(7, Arrays.asList(2, 4)));
+        System.out.println(s.dynamicHowSum(8, Arrays.asList(2,3,5)));
+        System.out.println(s.dynamicHowSum(300, Arrays.asList(7,14)));
+    }
+
+    private static void bestSum() {
+        Sum s = new Sum();
+        System.out.println("Naive can sum:");
+        System.out.println(s.naiveBestSum(7, Arrays.asList(5,3,4,7)));
+        System.out.println(s.naiveBestSum(8, Arrays.asList(3,5)));
+        System.out.println(s.naiveBestSum(8, Arrays.asList(1,4,5)));
+        // System.out.println(s.naiveBestSum(100, Arrays.asList(1,2,5,25)));
+        
+        System.out.println("\nDynamic best sum:");
+        System.out.println(s.dynamicBestSum(7, Arrays.asList(5,3,4,7)));
+        System.out.println(s.dynamicBestSum(8, Arrays.asList(3,5)));
+        System.out.println(s.dynamicBestSum(8, Arrays.asList(1,4,5)));
+        System.out.println(s.dynamicBestSum(100, Arrays.asList(1,2,5,25)));
+    }
+}
---
+++ Java/Sum.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Sum {
+    public Sum() {
+
+    }
+
+    // Decision Problem
+    public boolean naiveCanSum(int targetSum, List<Integer> numbers) {
+        if (targetSum < 0) { return false; }
+        if (targetSum == 0) { return true; }
+        for (Integer value : numbers) {
+            if (naiveCanSum(targetSum - value, numbers)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean dynamicCanSum(int targetSum, List<Integer> numbers) {
+        return dynamicCanSum(targetSum, numbers, new HashMap<>());
+    }
+
+    private boolean dynamicCanSum(int targetSum, List<Integer> numbers, HashMap<Integer, Boolean> memo) {
+        if (memo.containsKey(targetSum)) { return memo.get(targetSum); }
+        if (targetSum < 0) { return false; }
+        if (targetSum == 0) { return true; }
+        for (Integer value : numbers) {
+            if (dynamicCanSum(targetSum - value, numbers, memo)) {
+                memo.put(targetSum, true);
+                return true;
+            }
+        }
+        memo.put(targetSum, false);
+        return false;
+    }
+
+    // Combinatoric Problem
+    public List<Integer> naiveHowSum(int targetSum, List<Integer> numbers) {
+        if (targetSum < 0) { return null; }
+        if (targetSum == 0) { return (new ArrayList<>()); }
+        for (Integer value : numbers) {
+            List<Integer> result = naiveHowSum(targetSum - value, numbers);
+            if (result != null) {
+                result.add(0, value);
+                return result;
+            }
+        }
+        return null;
+    }
+
+    public List<Integer> dynamicHowSum(int targetSum, List<Integer> numbers) {
+        return dynamicHowSum(targetSum, numbers, new HashMap<>());
+    }
+
+    private List<Integer> dynamicHowSum(int targetSum, List<Integer> numbers, HashMap<Integer, List<Integer>> memo) {
+        if (memo.containsKey(targetSum)) { return memo.get(targetSum); }
+        if (targetSum < 0) { return null; }
+        if (targetSum == 0) { return (new ArrayList<>()); }
+        // System.out.println(memo);
+        for (Integer value : numbers) {
+            List<Integer> result = dynamicHowSum(targetSum - value, numbers, memo);
+            if (result != null) {
+                result.add(0, value);
+                memo.put(targetSum, result);
+                return result;
+            }
+        }
+        memo.put(targetSum, null);
+        return null;
+    }
+
+    // Optimization Problem
+    public List<Integer> naiveBestSum(int targetSum, List<Integer> numbers) {
+        if (targetSum < 0) { return null; }
+        if (targetSum == 0) { return new ArrayList<>(); }
+        List<Integer> shortest = null;
+        for (Integer value : numbers)  {
+            List<Integer> combination = naiveBestSum(targetSum - value, numbers);
+            if (combination != null) {
+                combination.add(0, value);
+                if (shortest == null || shortest.size() > combination.size()) {
+                    shortest = combination;
+                }
+            }
+        }
+        return shortest;
+    }
+
+    public List<Integer> dynamicBestSum(int targetSum, List<Integer> numbers) {
+        return dynamicBestSum(targetSum, numbers, new HashMap<>());
+    }
+    
+    private List<Integer> dynamicBestSum(int targetSum, List<Integer> numbers, HashMap<Integer, List<Integer>> memo) {
+        if (memo.containsKey(targetSum)) { return memo.get(targetSum); }
+        if (targetSum < 0) { return null; }
+        if (targetSum == 0) { return new ArrayList<>(); }
+        List<Integer> shortest = null;
+        for (Integer value : numbers)  {
+            List<Integer> combination = dynamicBestSum(targetSum - value, numbers, memo);
+            if (combination != null) {
+                List<Integer> list = new ArrayList<>();
+                list.add(value);
+                list.addAll(combination);
+                if (shortest == null || shortest.size() > list.size()) {
+                    shortest = list;
+                }
+            }
+        }
+        memo.put(targetSum, shortest);
+        return shortest;
+    }
+
+}
