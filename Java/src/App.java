3641922d4d0448fa9cfdb9280a505962ef43478e
LuizGuerra <luizpedrofg@gmail.com>
2021-02-12 01:19:11 -0300

    major read me structure and content updates

--- Java/src/App.java
+++ Java/src/App.java
@@ -72,7 +72,7 @@ public class App {
         System.out.println(s.dynamicCanSum(8, Arrays.asList(2,3,5)));
         System.out.println(s.dynamicCanSum(300, Arrays.asList(7,14)));
         
-        System.out.println("\nDynamic can sum with tabularization:");
+        System.out.println("\nDynamic can sum with tabulation:");
         System.out.println(s.dynamicCanSumTabular(7, Arrays.asList(2, 3)));
         System.out.println(s.dynamicCanSumTabular(7, Arrays.asList(5,3,4,7)));
         System.out.println(s.dynamicCanSumTabular(7, Arrays.asList(2,4)));
@@ -96,7 +96,7 @@ public class App {
         System.out.println(s.dynamicHowSumMemo(8, Arrays.asList(2,3,5)));
         System.out.println(s.dynamicHowSumMemo(300, Arrays.asList(7,14)));
         
-        System.out.println("\nDynamic how sum with tabularization:");
+        System.out.println("\nDynamic how sum with tabulation:");
         System.out.println(s.dynamicHowSumTabular(7, Arrays.asList(2, 3)));
         System.out.println(s.dynamicHowSumTabular(7, Arrays.asList(5,3,4,7)));
         System.out.println(s.dynamicHowSumTabular(7, Arrays.asList(2, 4)));
@@ -118,7 +118,7 @@ public class App {
         System.out.println(s.dynamicBestMemo(8, Arrays.asList(1,4,5)));
         System.out.println(s.dynamicBestMemo(100, Arrays.asList(1,2,5,25)));
         
-        System.out.println("\nDynamic best sum with tabularization:");
+        System.out.println("\nDynamic best sum with tabulation:");
         System.out.println(s.dynamicBestSumTabular(7, Arrays.asList(5,3,4,7)));
         System.out.println(s.dynamicBestSumTabular(8, Arrays.asList(3,5)));
         System.out.println(s.dynamicBestSumTabular(8, Arrays.asList(1,4,5)));
@@ -139,7 +139,7 @@ public class App {
             "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
              Arrays.asList("e","ee","eee","eeee","eeeeee","eeeeeee","eeeeeeee")));
         
-        System.out.println("\nDynamic construct with tabularization:");
+        System.out.println("\nDynamic construct with tabulation:");
         System.out.println(c.dynamicCanConstructTabular("skate", Arrays.asList("sk", "ate")));
         System.out.println(c.dynamicCanConstructTabular("abcdef", Arrays.asList("ab","cd","abc","bc","de","ef")));
         System.out.println(c.dynamicCanConstructTabular("abcdef", Arrays.asList("ab","cd","abc","bc","f")));
@@ -164,7 +164,7 @@ public class App {
             "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
              Arrays.asList("e","ee","eee","eeee","eeeeee","eeeeeee","eeeeeeee")));
         
-        System.out.println("\nDynamic construct with tabularization:");
+        System.out.println("\nDynamic construct with tabulation:");
         System.out.println(c.dynamicCountConstructTabular("purple", Arrays.asList("purp","p","ur","le","purpl")));
         System.out.println(c.dynamicCountConstructTabular("abcdef", Arrays.asList("ab","abc","cd","def","abcd")));
         System.out.println(c.dynamicCountConstructTabular("skateboard", Arrays.asList("bo","rd","ate","t","ska","sk","boar")));
@@ -191,7 +191,7 @@ public class App {
             "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
              Arrays.asList("e","ee","eee","eeee","eeeeee"))); //0
         
-        System.out.println("\nDynamic construct with tabularization:");
+        System.out.println("\nDynamic construct with tabulation:");
         System.out.println(c.dynamicAllConstructTabular("purple", Arrays.asList("purp","p","ur","le","purpl"))); //2
         System.out.println(c.dynamicAllConstructTabular("abcdef", Arrays.asList("ab","abc","cd","def","abcd"))); //1
         System.out.println(c.dynamicAllConstructTabular("skateboard", Arrays.asList("bo","rd","ate","t","ska","sk","boar"))); //0
--- README.md
+++ README.md
@@ -1,19 +1,49 @@
 # Dynamic Programming
 
-This is the dynamic implementations of commong software intensive problems.
+## Introduction
 
-## Fibonacci
+In this repo, there are 3 different kinds of implementations of common software-intensive problems (Fibonacci, Grid Traveler, Sum Problems and Construct Problems):
+1. Naive implementation;
+2. Dynamic implementation with [memoization](https://en.wikipedia.org/wiki/Memoization);
+3. Dynamic implementation with tabulation.
 
-## Grid Traveler
+I couldn't find any sources only explaining tabulation, but [here](https://programming.guide/dynamic-programming-vs-memoization-vs-tabulation.html) is a good source explaining both approaches. 
 
-## Sum Problems
+More info about dynamic programming can be found [here](https://en.wikipedia.org/wiki/Dynamic_programming).
 
-## Cosntruct Problems
+The study was guided by [this class](https://youtu.be/oBt53YbR9Kk) using JavaScript. This made me want to try to implement the same problems with different languages: Java, Swift, and Haskell. Currently, this is an ongoing project.
 
+## Problems
+
+### Fibonacci
+
+Consists of the implementation of the [Fibonacci Sequence](https://en.wikipedia.org/wiki/Fibonacci_number), where, given a number, the function calculates the value of passed index into the sequence.
+
+### Grid Traveller
+
+Given a matrix with size M and N, how many paths can you find from point (x: 0, y: 0) to (x: M, y: N), with the only possible moves being going right and down.
+
+### Sum Problems
+
+Similar problems, but with different focuses. All 3 problems have the same 2 inputs: target number and a list of numbers. Here, we try to find if we can achieve the target number by adding numbers from the list.
+
+1. Can Sum: can sum numbers from the list to achieve the target number
+2. How Sum: a sequence of numbers to be added to achieve a target number
+3. All Sum: all possible sequence of numbers that when added, gives the target number
+
+### Construct Problems
+
+Similar to the Sum problems, here we have a target word to achieve and a list of substrings that we can use as we please.
+
+1. Can Construct: if a word can be created given a list of strings
+2. Count Construct: how many different ways a word can be created given a list of strings
+3. All Constructs: which different ways a word can be created givena list of strings
+
+## Implementation
 
 Problem | Java | Swift | Haskell
 --- | --- | --- | ---
-Fibonacci | [Link](https://github.com/LuizGuerra/Dynamic-Programming/blob/main/Java/src/Fibonacci.java) | - | -
+Fibonacci | [Link](https://github.com/LuizGuerra/Dynamic-Programming/blob/main/Java/src/Fibonacci.java) | [Link](https://github.com/LuizGuerra/Dynamic-Programming/blob/main/Swift/Fibonacci.java) | -
 Grid  | [Link](https://github.com/LuizGuerra/Dynamic-Programming/blob/main/Java/src/GridTraveler.java) | - | -
 Sum problems | [Link](https://github.com/LuizGuerra/Dynamic-Programming/blob/main/Java/src/Sum.java) | - | -
 Construct problems | [Link](https://github.com/LuizGuerra/Dynamic-Programming/blob/main/Java/src/Construct.java) | - | -
