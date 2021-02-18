1a610043265768452caf4f15ceb79f7537535e45
LuizGuerra <luizpedrofg@gmail.com>
2021-02-18 01:33:26 -0300

    IO

--- Haskell/Fibonatti.hs
+++ Haskell/Fibonatti.hs
@@ -1,4 +1,19 @@
-fibonacci :: Int -> Int
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = (fibonacci (n-1)) + (fibonacci (n-2))
+import Data.List
+
+naiveFib :: Int -> Int
+naiveFib 0 = 0
+naiveFib 1 = 1
+naiveFib n = (naiveFib (n-1)) + (naiveFib (n-2))
+
+
+
+
+concat' :: [Int] -> String
+concat' [] = ""
+concat' (x:xs:[]) = show x ++ ", " ++ show xs
+concat' (x:xs) = show x ++ ", " ++ concat' xs
+
+main :: IO()
+main = do
+    putStrLn "Fibonacci naive implementation"
+    putStrLn (concat' (map naiveFib [1..10]))
