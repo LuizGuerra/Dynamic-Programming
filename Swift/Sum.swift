c4c15af7d51d66ff6759429237586337e67b478f
LuizGuerra <luizpedrofg@gmail.com>
2021-02-19 00:07:30 -0300

    swift how sum

--- Swift/Sum.swift
+++ Swift/Sum.swift
@@ -69,6 +69,68 @@ func dynamicMemoCanSum(targetSum: Int, numbers: [Int]) -> Bool {
 * ===============================================================
 */
 
+func naiveHowSum(targetSum: Int, numbers: [Int]) -> [Int]? {
+    if targetSum < 0 {
+        return nil
+    }
+    if targetSum == 0 {
+        return []
+    }
+    for number in numbers {
+        guard var result = naiveHowSum(targetSum: targetSum - number, numbers: numbers) else { continue }
+        result.append(number)
+        return result
+    }
+    return nil
+}
+
+func dynamicMemoHowSum(targetSum: Int, numbers: [Int]) -> [Int]? {
+    var memo: [Int:[Int]?] = [:]
+    func howSum(_ targetSum: Int, _ numbers: [Int], _ memo: inout [Int:[Int]?]) -> [Int]? {
+        if let value = memo[targetSum] {
+            return value
+        }
+        if targetSum < 0 {
+            return nil
+        }
+        if targetSum == 0 {
+            return []
+        }
+        for number in numbers {
+            guard var result = howSum(targetSum - number, numbers, &memo) else { continue }
+            result.append(number)
+            memo[targetSum] = result
+            return result
+        }
+        memo[targetSum] =  nil
+        return nil
+    }
+    return howSum(targetSum, numbers, &memo)
+}
+
+func dynamicTabularHowSum (targetSum: Int, numbers: [Int]) -> [Int]? {
+    var tabulation: [[Int]?] = [[]]
+    for _ in 0 ..< targetSum {
+        tabulation.append(nil)
+    }
+    for i in 0 ... targetSum {
+        for number in numbers {
+            let index = i+number
+            guard let array = tabulation[i] else { continue }
+            if index > targetSum {
+                continue
+            }
+
+            let list = [number] + array
+            if index == targetSum {
+                return list
+            }
+            tabulation[index] = list
+        }
+    }
+    return nil
+}
+
 /**
 * ===============================================================
 *                          HOW SUM  
